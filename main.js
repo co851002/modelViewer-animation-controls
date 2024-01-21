33fcbb416e82594167922379100ae420d0621abb
co851002 <co851002@gmail.com>
2024-01-21 00:13:24 +0000

    Audio added

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import './style.css';
-import { modelViewer } from './modelViewer';
-
+import { modelViewer, waveSurfer } from './modelViewer';
 modelViewer(document.querySelector('#container'));
+waveSurfer(document.querySelector('#container')); 
+
+const config = {
+    modelUrl: '',
+    audioUrl: '',
+    width: '',
+    height: ''
+}
+
+window.mvConfig = config;
--- modelViewer.js
+++ modelViewer.js
@@ -1,5 +1,7 @@
 import '@google/model-viewer';
+import WaveSurfer from 'wavesurfer.js'
 
+let wavesurfer;
 export function modelViewer(element) {
   const modelViewer = document.createElement('model-viewer');
   modelViewer.src =
@@ -8,12 +10,15 @@ export function modelViewer(element) {
   // modelViewer.setAttribute('auto-rotate', '');
   modelViewer.setAttribute('camera-controls', '');
   // modelViewer.setAttribute('autoplay', false);
+  // modelViewer.setAttribute('scale', '1 1 1'); // Replace with your desired scale values
   modelViewer.style.width = window.innerWidth;
   modelViewer.style.height = window.innerHeight;
   modelViewer.shadowIntensity = 1;
   modelViewer.touchAction = 'pan-y';
   modelViewer.ar = true;
   modelViewer.arMode = 'webxr scene-viewer';
+  // modelViewer.setAttribute('field-of-view', '9deg');
+  // modelViewer.setAttribute('camera-orbit', '0deg 75deg 150%');
 
   //Add modelviewer to #container in #app
   element.appendChild(modelViewer);
@@ -32,10 +37,78 @@ export function modelViewer(element) {
     if (!paused) {
       modelViewer.pause();
       btn.innerHTML = 'Play';
+      fadeOut()
     } else {
       modelViewer.play();
       btn.innerHTML = 'Pause';
+      fadeIn()
     }
     console.log('play');
   });
 }
+
+export function waveSurfer(element) {
+
+  wavesurfer = WaveSurfer.create({
+    container: '#container',
+    waveColor: '#3d90b6',
+    progressColor: '#f0f0f0',
+    url: '/cool_music_3.mp3',
+    // Set a bar width
+    barHeight: 0.5,
+    // Set a bar width
+    barWidth: 1,
+    // Optionally, specify the spacing between bars
+    barGap: 2,
+    // And the bar radius
+    barRadius: 2,
+    loop: false
+  });
+
+
+  // Handle the finish event
+  wavesurfer.on('finish', function () {
+    console.log('here')
+    fadeIn(); // Call fadeIn function to restart the audio
+  });
+
+
+}
+
+
+
+function fadeIn(duration = 300) {
+  let startVolume = 0;
+  wavesurfer.setVolume(startVolume);
+  wavesurfer.play();
+
+  const interval = 30;
+  const step = interval / duration;
+
+  let intervalId = setInterval(() => {
+    startVolume += step;
+    if (startVolume >= 1) {
+      startVolume = 1;
+      clearInterval(intervalId);
+    }
+    wavesurfer.setVolume(startVolume);
+  }, interval);
+}
+
+function fadeOut(duration = 300) {
+  let currentVolume = wavesurfer.getVolume();
+
+  const interval = 30;
+  const step = currentVolume * (interval / duration);
+
+  let intervalId = setInterval(() => {
+    currentVolume -= step;
+    if (currentVolume <= 0) {
+      currentVolume = 0;
+      clearInterval(intervalId);
+      wavesurfer.pause();
+    }
+    wavesurfer.setVolume(currentVolume);
+  }, interval);
+}
+
